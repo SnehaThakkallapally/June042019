7ec5b251041b3def6d92a58bece7ed34639868eb
Sneha <tsbsdev@gmail.com>
2019-06-10 13:56:31 -0400

    I have added my first python program first.py

---
+++ first.py
@@ -0,0 +1,14 @@
+print('Hello my name is "Sneha". I love coding')
+print("This is 'my first program'.")
+
+input("x")
+input("y")
+input("z")
+ 
+print("value1 = x")
+print("value2 = y")
+print("value3 = z")
+
+print(type("x"))
+print(type("y"))
+print(type("z"))
